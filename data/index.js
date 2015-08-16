542cb45aeb741f2ed00d2993cc3034d243939f73
simontaosim <xsqfeather@126.com>
2015-08-17 01:56:41 +0800

    first commit

---
+++ data/index.js
@@ -0,0 +1,6 @@
+var promotions=[
+
+{"title":"七夕送票，助力约炮","description":"明媚影城（大足印象店）七夕大优惠，凡在大足商场消费满300元者，免费送一张电影票",
+"created_at":"2015-8-2 18:00:45","wored_begin":"2015-8-18 16:00:00", "worked_until":"2015-8-23 18:00:00","banner_url": "index_images_banner1.png"}
+
+];
