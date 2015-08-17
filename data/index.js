3cbe15c7451cde3dabb463367c184ce841c667f1
simontaosim <xsqfeather@126.com>
2015-08-17 23:18:01 +0800

    merge pc at home

--- data/index.js
+++ data/index.js
@@ -1,6 +1,15 @@
 var promotions=[
 
 {"title":"七夕送票，助力约炮","description":"明媚影城（大足印象店）七夕大优惠，凡在大足商场消费满300元者，免费送一张电影票",
-"created_at":"2015-8-2 18:00:45","wored_begin":"2015-8-18 16:00:00", "worked_until":"2015-8-23 18:00:00","banner_url": "index_images_banner1.png"}
+"rule": "明媚影城（大足印象店）七夕大优惠，凡在大足商场用此APP付款消费满300元者，免费送一张电影票"，
+"created_at":"2015-8-2 18:00:45","wored_begin":"2015-8-18 16:00:00", "worked_until":"2015-8-23 18:00:00",
+"banner_url": "index_images_banner1.png", "quantity": "unlimited", "rest_quantity": "unlimited",
+"created_by": "store1"},
+
+{"title":"SELECTED 100元抵用","description":"SELECTED老顾客大回馈！", "rule": "凡在SELECTED大足印象专卖点，用此APP付款超过300元的商品可使用此抵用券",
+"created_at":"2015-8-2 18:00:45","wored_begin":"2015-8-18 16:00:00", "worked_until":"2015-8-23 18:00:00",
+"banner_url": "index_images_banner1.png", "quantity": "1400", "rest_quantity": "10000", "rest_quantity": "4502",
+"created_by": "store2"}
+
 
 ];
